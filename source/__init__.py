b3d7ef095e3429e095807c58647a1f1cc16fbd09
Ryan Gilera <ryangilera@gmail.com>
2014-05-03 16:01:03 +0100

    Add input tile mechanism in the canvas

--- source/__init__.py
+++ source/__init__.py
@@ -7,41 +7,215 @@ import classes
 WIDTH = 700
 HEIGHT = 600
 
+x_pos_puzzle = 50
+y_pos_puzzle = 100
+x_pos_input = 400
+y_pos_input = 100
+tile_wide = 70
+
+tile_counter_input = 1
+tile_list_input = []
+
+color_input_bg = 'White'
+color_puzzle_bg = 'White'
+color_blank_tile = 'Teal'
+
+isInputOn = True
+isTile1LockOn = True
+isTile2LockOn = True
+isTile3LockOn = True
+isTile4LockOn = True
+isTile5LockOn = True
+isTile6LockOn = True
+isTile7LockOn = True
+isTile8LockOn = True
+isTile9LockOn = True
 
 def draw(canvas):
+    # Positions for puzzle tiles
+    x = x_pos_puzzle
+    y = y_pos_puzzle
 
-    x = 50
-    y = 100
-    s = 70
-    color = 'White'
-    canvas.draw_polygon([[x, y], [x, y+s], [x+s, y+s], [x+s, y]], 2, 'Black', 'White')
-    canvas.draw_polygon([[x+s, y], [x+s,y+s], [x+(2*s),y+s], [x+(2*s),y]], 2, 'Black', 'White')
-    canvas.draw_polygon([[x+(2*s),y], [x+(2*s),y+s], [x+(3*s),y+s], [x+(3*s),y]], 2, 'Black', 'White')
+    # Square size
+    s = tile_wide
 
-    canvas.draw_polygon([[x, y+s], [x, y+(2*s)], [x+s, y+(2*s)], [x+s, y+s]], 2, 'Black', 'White')
-    canvas.draw_polygon([[x+s, y+s], [x+s, y+(2*s)], [x+(2*s), y+(2*s)], [x+(2*s), y+s]], 2, 'Black', 'Teal')
-    canvas.draw_polygon([[x+(2*s), y+s], [x+(2*s), y+(2*s)], [x+(3*s), y+(2*s)], [x+(3*s), y+s]], 2, 'Black', 'White')
+    # Draw puzzle tiles
+    canvas.draw_polygon([[x, y], [x, y+s], [x+s, y+s], [x+s, y]], 2, 'Black', color_puzzle_bg)
+    canvas.draw_polygon([[x+s, y], [x+s,y+s], [x+(2*s),y+s], [x+(2*s),y]], 2, 'Black', color_puzzle_bg)
+    canvas.draw_polygon([[x+(2*s),y], [x+(2*s),y+s], [x+(3*s),y+s], [x+(3*s),y]], 2, 'Black', color_puzzle_bg)
 
-    canvas.draw_polygon([[x, y+(2*s)], [x, y+(3*s)], [x+s, y+(3*s)], [x+s, y+(2*s)]], 2, 'Black', 'White')
-    canvas.draw_polygon([[x+s, y+(2*s)], [x+s, y+(3*s)], [x+(2*s), y+(3*s)], [x+(2*s), y+(2*s)]], 2, 'Black', 'White')
-    canvas.draw_polygon([[x+(2*s), y+(2*s)], [x+(2*s), y+(3*s)], [x+(3*s), y+(3*s)], [x+(3*s), y+(2*s)]], 2, 'Black', 'White')
+    canvas.draw_polygon([[x, y+s], [x, y+(2*s)], [x+s, y+(2*s)], [x+s, y+s]], 2, 'Black', color_puzzle_bg)
+    canvas.draw_polygon([[x+s, y+s], [x+s, y+(2*s)], [x+(2*s), y+(2*s)], [x+(2*s), y+s]], 2, 'Black', color_puzzle_bg)
+    canvas.draw_polygon([[x+(2*s), y+s], [x+(2*s), y+(2*s)], [x+(3*s), y+(2*s)], [x+(3*s), y+s]], 2, 'Black', color_puzzle_bg)
 
-    v = 440
-    w = 100
-    canvas.draw_polygon([[v, w], [v, w+s], [v+s, w+s], [v+s, w]], 2, 'Black', color)
-    canvas.draw_polygon([[v+s, w], [v+s,w+s], [v+(2*s),w+s], [v+(2*s),w]], 2, 'Black', color)
-    canvas.draw_polygon([[v+(2*s),w], [v+(2*s),w+s], [v+(3*s),w+s], [v+(3*s),w]], 2, 'Black', color)
+    canvas.draw_polygon([[x, y+(2*s)], [x, y+(3*s)], [x+s, y+(3*s)], [x+s, y+(2*s)]], 2, 'Black', color_puzzle_bg)
+    canvas.draw_polygon([[x+s, y+(2*s)], [x+s, y+(3*s)], [x+(2*s), y+(3*s)], [x+(2*s), y+(2*s)]], 2, 'Black', color_puzzle_bg)
+    canvas.draw_polygon([[x+(2*s), y+(2*s)], [x+(2*s), y+(3*s)], [x+(3*s), y+(3*s)], [x+(3*s), y+(2*s)]], 2, 'Black', color_puzzle_bg)
 
-    canvas.draw_polygon([[v, w+s], [v, w+(2*s)], [v+s, w+(2*s)], [v+s, w+s]], 2, 'Black', color)
-    canvas.draw_polygon([[v+s, w+s], [v+s, w+(2*s)], [v+(2*s), w+(2*s)], [v+(2*s), w+s]], 2, 'Black', color)
-    canvas.draw_polygon([[v+(2*s), w+s], [v+(2*s), w+(2*s)], [v+(3*s), w+(2*s)], [v+(3*s), w+s]], 2, 'Black', color)
+    # Positions for input tiles
+    v = x_pos_input
+    w = y_pos_input
 
-    canvas.draw_polygon([[v, w+(2*s)], [v, w+(3*s)], [v+s, w+(3*s)], [v+s, w+(2*s)]], 2, 'Black', color)
-    canvas.draw_polygon([[v+s, w+(2*s)], [v+s, w+(3*s)], [v+(2*s), w+(3*s)], [v+(2*s), w+(2*s)]], 2, 'Black', color)
-    canvas.draw_polygon([[v+(2*s), w+(2*s)], [v+(2*s), w+(3*s)], [v+(3*s), w+(3*s)], [v+(3*s), w+(2*s)]], 2, 'Black', color)
+    # Positions for drawing the number
+    v_centre = ((v + v + s) / 2) - 10
+    w_centre = ((w + w + s) / 2) + 15
+
+    if isTile1LockOn is True:
+        canvas.draw_polygon([[v, w], [v, w + s], [v + s, w + s], [v + s, w]], 2, 'Black', color_input_bg)
+        canvas.draw_text('1', (v_centre, w_centre), 40, 'Black', 'monospace')
+
+    if isTile2LockOn is True:
+        canvas.draw_polygon([[v+s, w], [v+s, w+s], [v+(2*s), w+s], [v+(2*s), w]], 2, 'Black', color_input_bg)
+        canvas.draw_text('2', (v_centre + s, w_centre), 40, 'Black', 'monospace')
+
+    if isTile3LockOn is True:
+        canvas.draw_polygon([[v+(2*s), w], [v+(2*s), w+s], [v+(3*s), w+s], [v+(3*s), w]], 2, 'Black', color_input_bg)
+        canvas.draw_text('3', (v_centre + (2*s), w_centre), 40, 'Black', 'monospace')
+
+    if isTile4LockOn is True:
+        canvas.draw_polygon([[v, w+s], [v, w+(2*s)], [v+s, w+(2*s)], [v+s, w+s]], 2, 'Black', color_input_bg)
+        canvas.draw_text('4', (v_centre, w_centre+s), 40, 'Black', 'monospace')
+
+    if isTile5LockOn is True:
+        canvas.draw_polygon([[v+s, w+s], [v+s, w+(2*s)], [v+(2*s), w+(2*s)], [v+(2*s), w+s]], 2, 'Black', color_input_bg)
+        canvas.draw_text('5', (v_centre + s, w_centre + s), 40, 'Black', 'monospace')
+
+    if isTile6LockOn is True:
+        canvas.draw_polygon([[v+(2*s), w+s], [v+(2*s), w+(2*s)], [v+(3*s), w+(2*s)], [v+(3*s), w+s]], 2, 'Black', color_input_bg)
+        canvas.draw_text('6', (v_centre + (2*s), w_centre + s), 40, 'Black', 'monospace')
+
+    if isTile7LockOn is True:
+        canvas.draw_polygon([[v, w+(2*s)], [v, w+(3*s)], [v+s, w+(3*s)], [v+s, w+(2*s)]], 2, 'Black', color_input_bg)
+        canvas.draw_text('7', (v_centre, w_centre + (2*s)), 40, 'Black', 'monospace')
+
+    if isTile8LockOn is True:
+        canvas.draw_polygon([[v+s, w+(2*s)], [v+s, w+(3*s)], [v+(2*s), w+(3*s)], [v+(2*s), w+(2*s)]], 2, 'Black', color_input_bg)
+        canvas.draw_text('8', (v_centre + s, w_centre + (2*s)), 40, 'Black', 'monospace')
+
+    if isTile9LockOn is True:
+        canvas.draw_polygon([[v+(2*s), w+(2*s)], [v+(2*s), w+(3*s)], [v+(3*s), w+(3*s)], [v+(3*s), w+(2*s)]], 2, 'Black', color_blank_tile)
+
+    # Draw from selected input tiles
+    if isInputOn is True:
+        x_centre = ((x + x + s) / 2) - 10
+        y_centre = ((y + y + s) / 2) + 15
+        draw_tile_counter = 1
+
+        for tile_num in tile_list_input:
+            x_draw = x_centre
+            y_draw = y_centre
+
+            if draw_tile_counter is 2:
+                x_draw = x_centre + s
+            elif draw_tile_counter is 3:
+                x_draw = x_centre + (2 * s)
+            elif draw_tile_counter is 4:
+                y_draw = y_centre + s
+            elif draw_tile_counter is 5:
+                x_draw = x_centre + s
+                y_draw = y_centre + s
+            elif draw_tile_counter is 6:
+                x_draw = x_centre + (2 * s)
+                y_draw = y_centre + s
+            elif draw_tile_counter is 7:
+                y_draw = y_centre + (2 * s)
+            elif draw_tile_counter is 8:
+                x_draw = x_centre + s
+                y_draw = y_centre + (2 * s)
+            elif draw_tile_counter is 9:
+                x_draw = x_centre + (2 * s)
+                y_draw = y_centre + (2 * s)
+            if tile_num is not '0':
+                canvas.draw_text(tile_num, (x_draw, y_draw), 40, 'Black', 'monospace')
+
+            draw_tile_counter += 1
+
+    return  None
+
+
+def mouse_handler_input(pos):
+    global tile_counter_input, isInputOn, tile_list_input
+    global isTile1LockOn, isTile2LockOn, isTile3LockOn, isTile4LockOn, isTile5LockOn, isTile6LockOn
+    global isTile7LockOn, isTile8LockOn, isTile9LockOn
+
+    #print "tile_list_input:", tile_list_input
+    if (isInputOn is True) and (tile_counter_input < 10):
+
+        if isTile1LockOn is True:
+            if (pos[0] < (x_pos_input + tile_wide) and pos[0] > x_pos_input) and \
+                    (pos[1] < (y_pos_input + tile_wide) and pos[1] > y_pos_input):
+                tile_list_input.append('1')
+                tile_counter_input += 1
+                isTile1LockOn = False
+                return None
+
+        if isTile2LockOn is True:
+            if (pos[0] < (x_pos_input + (2 * tile_wide)) and pos[0] > (x_pos_input + tile_wide)) and \
+                    (pos[1] < (y_pos_input + tile_wide) and pos[1] > y_pos_input):
+                tile_list_input.append('2')
+                tile_counter_input += 1
+                isTile2LockOn = False
+                return None
+
+        if isTile3LockOn is True:
+            if (pos[0] < (x_pos_input + (3 * tile_wide)) and pos[0] > (x_pos_input + (2 * tile_wide))) and \
+                    (pos[1] < (y_pos_input + tile_wide) and pos[1] > y_pos_input):
+                tile_list_input.append('3')
+                tile_counter_input += 1
+                isTile3LockOn = False
+                return None
+
+        if isTile4LockOn is True:
+            if (pos[0] < (x_pos_input + tile_wide) and pos[0] > x_pos_input) and \
+                    (pos[1] < (y_pos_input + (2 * tile_wide)) and pos[1] > (y_pos_input + tile_wide)):
+                tile_list_input.append('4')
+                tile_counter_input += 1
+                isTile4LockOn = False
+                return None
+
+        if isTile5LockOn is True:
+             if (pos[0] < (x_pos_input + (2 * tile_wide)) and pos[0] > (x_pos_input + tile_wide)) and \
+                    (pos[1] < (y_pos_input + (2 * tile_wide)) and pos[1] > (y_pos_input + tile_wide)):
+                tile_list_input.append('5')
+                tile_counter_input += 1
+                isTile5LockOn = False
+                return None
+
+        if isTile6LockOn is True:
+             if (pos[0] < (x_pos_input + (3 * tile_wide)) and pos[0] > (x_pos_input + (2 * tile_wide))) and \
+                    (pos[1] < (y_pos_input + (2 * tile_wide)) and pos[1] > (y_pos_input + tile_wide)):
+                tile_list_input.append('6')
+                tile_counter_input += 1
+                isTile6LockOn = False
+                return None
+
+        if isTile7LockOn is True:
+             if (pos[0] < (x_pos_input + tile_wide) and pos[0] > x_pos_input) and \
+                    (pos[1] < (y_pos_input + (3 * tile_wide)) and pos[1] > (y_pos_input + (2 * tile_wide))):
+                tile_list_input.append('7')
+                tile_counter_input += 1
+                isTile7LockOn = False
+                return None
+
+        if isTile8LockOn is True:
+             if (pos[0] < (x_pos_input + (2 * tile_wide)) and pos[0] > (x_pos_input + tile_wide)) and \
+                    (pos[1] < (y_pos_input + (3 * tile_wide)) and pos[1] > (y_pos_input + (2 * tile_wide))):
+                tile_list_input.append('8')
+                tile_counter_input += 1
+                isTile8LockOn = False
+                return None
+
+        if isTile9LockOn is True:
+             if (pos[0] < (x_pos_input + (3 * tile_wide)) and pos[0] > (x_pos_input + (2 * tile_wide))) and \
+                    (pos[1] < (y_pos_input + (3 * tile_wide)) and pos[1] > (y_pos_input + (2 * tile_wide))):
+                tile_list_input.append('0')
+                tile_counter_input += 1
+                isTile9LockOn = False
+                return None
 
 frame = simplegui.create_frame("8 Puzzle Solver", WIDTH, HEIGHT)
 frame.set_canvas_background('Silver')
 frame.set_draw_handler(draw)
+frame.set_mouseclick_handler(mouse_handler_input)
 
 frame.start()
