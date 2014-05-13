db50374fecd5ea5fb5d2ae92c8b447d95c0e0304
Ryan Gilera <ryangilera@gmail.com>
2014-05-13 17:40:15 +0100

    Add class method documentation, modify comments for readability

--- source/classes.py
+++ source/classes.py
@@ -2,6 +2,13 @@ import math
 
 class State:
     def __init__(self,node,start,goal,gn):
+        """
+        :description: The class constructor
+        :param node: The pattern or state using list of values (integers)
+        :param start: The initial state
+        :param goal: The goal state
+        :param gn: The g(n) value, cost it takes to reach this state/node from initial state
+        """
         self.start = start
         self.goal = goal
         self.node = node
@@ -12,8 +19,13 @@ class State:
         self.fn = self.gn + self.hn
 
     def generate_children(self,master_list):
-        # n is the element position
-        # t is the element value
+        """
+        :description: Generates children of current object by identifying possible tile moves.
+        :param master_list: List of objects generated from main program
+        :note:
+            n - the element position
+            t - the element value
+        """
         for n, t in enumerate(self.node):
             if t == 0:
                 if n == 0:
@@ -84,29 +96,48 @@ class State:
                     break
 
 
-    # Returns object list, later to be used to append to master_list
     def get_tiles(self):
+        """
+        :description: Returns object list, later to be used to append to master_list
+        :return: self.node
+        :rtype: List of integers
+        """
         return self.node
 
-    # Checks if the generated child is already in previous generated states (master_list)
-    # If not, add it to self.children
+
     def generate_child(self,a,b, val, states_list):
+        """
+        :description: Generates a state, a successor of current object, by moving one tile to the blank space
+            done by switching 0 (blank) with one tile being moved
+        :param a: A tile position where the blank space should be created
+        :param b: A tile position where the tile (val) moved into
+        :param val: The tile value that is being moved to the tile position (b)
+        :param states_list: List of objects generated in the main program. It is used to compare if the child generated
+            is already an object in this list. If not, acquire the generated successor as the child of the current
+            object. This makes sure that no child will be generated that has the same state as the current object's
+            parent has.
+        """
         del self.temp
         self.temp = list(self.node)
         self.temp[a] = 0
         self.temp[b] = val
         list_pattern_states = []
 
-        # state_list is a list of objects not list of patterns
-        # change list of objects to list of patterns
+        # make a list of states from a list of objects
         for state in states_list:
             list_pattern_states.append(state.node)
 
+        # Compares previous generated states with the new child generated
         if not self.temp in list_pattern_states:
             self.children.append(self.temp)
 
 
     def evaluate_heuristic(self):
+        """
+        :description: Evaluates h(n) value for each state object
+        :return: totalDist
+        :rtype : int
+        """
         misplacedTile = 0
         sumDist=0
 
