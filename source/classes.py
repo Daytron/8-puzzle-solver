c6d1b44be0d1a4ff95a578d84e66ee474394128e
Ryan Gilera <ryangilera@gmail.com>
2014-05-13 15:03:24 +0100

    Change  multiple If statements into elifs and remove unnecessary items

--- source/classes.py
+++ source/classes.py
@@ -7,8 +7,6 @@ class State:
         self.node = node
         self.gn = gn
         self.hn = self.evaluate_heuristic()
-        self.index = 0
-
         self.children = []
         self.temp = []
         self.fn = self.gn + self.hn
@@ -24,8 +22,7 @@ class State:
                     self.generate_child(1,0,a,master_list)
                     self.generate_child(3,0,b,master_list)
                     break
-
-                if n == 1:
+                elif n == 1:
                     a = self.node[0]
                     b = self.node[2]
                     c = self.node[4]
@@ -33,15 +30,13 @@ class State:
                     self.generate_child(2,1,b,master_list)
                     self.generate_child(4,1,c,master_list)
                     break
-
-                if n == 2:
+                elif n == 2:
                     a = self.node[1]
                     b = self.node[5]
                     self.generate_child(1,2,a,master_list)
                     self.generate_child(5,2,b,master_list)
                     break
-
-                if n == 3:
+                elif n == 3:
                     a = self.node[0]
                     b = self.node[4]
                     c = self.node[6]
@@ -49,8 +44,7 @@ class State:
                     self.generate_child(4,3,b,master_list)
                     self.generate_child(6,3,c,master_list)
                     break
-
-                if n == 4:
+                elif n == 4:
                     a = self.node[1]
                     b = self.node[3]
                     c = self.node[5]
@@ -60,8 +54,7 @@ class State:
                     self.generate_child(5,4,c,master_list)
                     self.generate_child(7,4,d,master_list)
                     break
-
-                if n == 5:
+                elif n == 5:
                     a = self.node[2]
                     b = self.node[4]
                     c = self.node[8]
@@ -69,15 +62,13 @@ class State:
                     self.generate_child(4,5,b,master_list)
                     self.generate_child(8,5,c,master_list)
                     break
-
-                if n == 6:
+                elif n == 6:
                     a = self.node[3]
                     b = self.node[7]
                     self.generate_child(3,6,a,master_list)
                     self.generate_child(7,6,b,master_list)
                     break
-
-                if n == 7:
+                elif n == 7:
                     a = self.node[6]
                     b = self.node[4]
                     c = self.node[8]
@@ -85,8 +76,7 @@ class State:
                     self.generate_child(4,7,b,master_list)
                     self.generate_child(8,7,c,master_list)
                     break
-
-                if n == 8:
+                elif n == 8:
                     a = self.node[5]
                     b = self.node[7]
                     self.generate_child(5,8,a,master_list)
@@ -115,8 +105,6 @@ class State:
         if not self.temp in list_pattern_states:
             self.children.append(self.temp)
 
-    def evaluate_gn(self):
-        pass
 
     def evaluate_heuristic(self):
         misplacedTile = 0
@@ -129,7 +117,6 @@ class State:
             sumDist +=math.fabs(self.node.index(i)-self.goal.index(i))
 
         totalDist = sumDist + misplacedTile
-
         return totalDist
 
 
